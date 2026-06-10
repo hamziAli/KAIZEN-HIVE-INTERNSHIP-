3ea41ecf6650c0aea28aada8bfc58f1901397aff
hamziAli <u2024203@giki.edu.pk>
2026-06-10 18:54:08 +0400

    Push the script to the folder

---
+++ TASK-05/task5-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Task 5: Git Deployment Command Sequence
+# Project Folder: web-project | Remote Branch: main
+
+git init
+git add .
+git commit -m "Initial web development project"
+git push origin main
